10a18b90743878f145502fce2ce0f0d9313759d4
malikinDmytro <dmytro.malikin@serious-software.com>
2021-01-24 15:06:09 +0200

    basic user friend request

--- src/authorization/v1/api.py
+++ src/authorization/v1/api.py
@@ -1,17 +1,13 @@
-from typing import Optional
+#!/usr/bin/python3
 from datetime import timedelta
 
+from config.settings import ACCESS_TOKEN_EXPIRE_MINUTES, ALGORITHM, SECRET_KEY
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-
 from jose import JWTError, jwt
-from passlib.context import CryptContext
-
-from config.settings import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
+from src.authorization.utils import authenticate_user, create_access_token, get_user
 
 from .models import Token, TokenData
-from src.authorization.utils import get_user, authenticate_user, create_access_token
-from src.user.v1.models import User
 
 router = APIRouter()
 
@@ -32,3 +28,23 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
         data={"sub": user["username"]}, expires_delta=access_token_expires
     )
     return {"access_token": access_token, "token_type": "bearer"}
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = await get_user(username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
